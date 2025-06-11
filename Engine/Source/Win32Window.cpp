af6d00f0ae52339379505138dcf2e31d3a912a08
Rebecca Sharkey <rebeccajaynesharkey@gmail.com>
2025-06-11 04:58:10 +0100

    Set up Window's window creation in engine

---
+++ Engine/Source/Win32Window.cpp
@@ -0,0 +1,60 @@
+﻿// © 2025 Ctrl Alt Delete Games. All rights reserved.
+
+#include "Win32Window.h"
+
+Win32Window::Win32Window(int width, int height, const char* title)
+{
+    WNDCLASS windowClass = {};
+    windowClass.lpfnWndProc = WindowProc;
+    windowClass.hInstance = GetModuleHandle(nullptr);
+    windowClass.lpszClassName = "CardGameWindow";
+
+    RegisterClass(&windowClass);
+
+    m_WindowHandle = CreateWindowEx(0, windowClass.lpszClassName, title,
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        CW_USEDEFAULT, CW_USEDEFAULT, width, height,
+        nullptr, nullptr, windowClass.hInstance, nullptr);
+
+    SetWindowLongPtr(m_WindowHandle, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
+}
+
+Win32Window::~Win32Window()
+{
+    DestroyWindow(m_WindowHandle);
+}
+
+HWND Win32Window::GetWindowHandle()
+{
+    return m_WindowHandle;
+}
+
+void Win32Window::PollEvents()
+{
+    MSG message;
+
+    while (PeekMessage(&message, nullptr, 0, 0, PM_REMOVE))
+    {
+        TranslateMessage(&message);
+        DispatchMessage(&message);
+
+        if (message.message == WM_QUIT)
+        {
+            m_WindowClosed = true;
+        }
+    }
+}
+
+bool Win32Window::ShouldClose()
+{
+    return m_WindowClosed;
+}
+
+LRESULT Win32Window::WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    if (message == WM_CLOSE) {
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
---
+++ Engine/Source/Win32Window.h
@@ -0,0 +1,22 @@
+﻿// © 2025 Ctrl Alt Delete Games. All rights reserved.
+
+#pragma once
+
+#include <Windows.h>
+
+class Win32Window {
+
+public:
+    Win32Window(int width, int height, const char* title);
+    ~Win32Window();
+
+    HWND GetWindowHandle();
+
+    void PollEvents();
+    bool ShouldClose();
+
+private:
+    HWND m_WindowHandle;
+    bool m_WindowClosed;
+    static LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+};
