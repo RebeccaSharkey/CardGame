f1feb6f3e90552497797a85b27052ee41a73c9b1
Rebecca Sharkey <rebeccajaynesharkey@gmail.com>
2025-06-11 04:58:49 +0100

    Create a window in our game using the engine's code and run it in main

---
+++ Game/Source/main.cpp
@@ -0,0 +1,15 @@
+// © 2025 Ctrl Alt Delete Games. All rights reserved.
+
+#include "Win32Window.h"
+#include <iostream>
+
+int main() {
+    Win32Window window(1280, 720, "Card Game");
+
+    while (!window.ShouldClose()) {
+        window.PollEvents();
+        // Here we'll render each frame
+    }
+
+    return 0;
+}
